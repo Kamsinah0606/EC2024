ee19c8014a338caaaf2302d4ef6cc75c9194f858
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-12 16:05:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ st.set_page_config(
 )
 
 st.header("Genetic Algorithm", divider="gray")
+
+import matplotlib.pyplot as plt
+
+gender_counts = df['Gender'].value_counts()
+
+plt.figure(figsize=(6, 6))
+plt.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=140)
+plt.title('Distribution of Gender')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.show()
