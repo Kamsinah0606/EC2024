154bb4066e2707fdf84d5341f4e35a05c6a60e41
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-15 15:15:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 
+st.set_page_config(
+    page_title="Genetic Algorithm"
+)
+
+st.header("Genetic Algorithm", divider="gray")
+
 # Set Streamlit page configuration
 st.set_page_config(layout="wide", title="Arts Faculty Data Analysis")
 
